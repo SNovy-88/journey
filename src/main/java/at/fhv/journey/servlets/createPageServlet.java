cf9e3848b572c2b0352db6414a7035708ade04eb
Sarah Neville <sarah.neville@students.fhv.at>
2023-11-26 00:09:57 +0100

    updated servlet to request parameter from the form

--- src/main/java/at/fhv/journey/servlets/createPageServlet.java
+++ src/main/java/at/fhv/journey/servlets/createPageServlet.java
@@ -3,7 +3,6 @@ package at.fhv.journey.servlets;
 import io.hypersistence.utils.hibernate.type.range.Range;
 import at.fhv.journey.hibernate.facade.DatabaseFacade;
 import at.fhv.journey.model.Hike;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,27 +16,31 @@ import java.time.LocalDate;
 @WebServlet(name = "createPageServlet", value = "/create_hike")
 public class createPageServlet extends HttpServlet {
     @Transactional
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        //Stepper 1
         String name = request.getParameter("nameInput");
         String description = request.getParameter("descInput");
-        int durationHour = 1;
-        int durationMin = 25;
-        BigDecimal distance = new BigDecimal("1.25");
-        int heightDifference = 950;
 
-        int fitnessLevel = 1;
-        int stamina = 1;
-        int experience = 1;
-        int scenery = 1;
+        //Stepper 2
+        int durationHour = Integer.parseInt(request.getParameter("duration-hr"));
+        int durationMin = Integer.parseInt(request.getParameter("duration-min"));
+        BigDecimal distance = BigDecimal.valueOf(Double.parseDouble(request.getParameter("distance")));
+        System.out.println(distance);
+        int heightDifference = Integer.parseInt(request.getParameter("height-difference"));
+
+        int fitnessLevel = Integer.parseInt(request.getParameter("physical-cond"));
+        int stamina = Integer.parseInt(request.getParameter("stamina"));
+        int experience = Integer.parseInt(request.getParameter("experience"));
+        int scenery = Integer.parseInt(request.getParameter("scenery"));
 
         Range<Integer> recommendedMonths = Range.closed(2, 5);
         String author = "testAuthor";
         LocalDate date = LocalDate.now();
 
         Hike hike = new Hike();
-//        hike.setHikeID(hikeId);
+//      hike.setHikeID(hikeId);
         hike.setName(name);
         hike.setDescription(description);
         hike.setDistance(distance);
