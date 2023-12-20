4f079836623942a0433bbb40806f9e6b30d3478f
Sarah Neville <sarah.neville@students.fhv.at>
2023-12-20 21:57:54 +0100

    added helper functions to set hiddenInput field for search string and dropdown functions

--- src/main/webapp/JS/filter.js
+++ src/main/webapp/JS/filter.js
@@ -1,20 +1,97 @@
-function filter() {
-    const fitness = document.getElementById("drop-down-btn-fitness")
-    const stamina = document.getElementById("stamina")
-    const experience = document.getElementById("experience")
-    const scenery = document.getElementById("scenery")
-    const months = document.querySelectorAll(".month")
-    let monthBits = 0;
 
+//sets the value of the hidden input field to the value of the search input field
+function setHiddenInput(){
+    console.log("setHiddenInput called");
+    let searchValue = document.getElementById("search-input").value;
+    document.getElementById("search-input-hidden").value = searchValue;
+}
 
-    months.forEach((month)=>{
-        if(month.checked) {
-            monthBits += parseInt(month.value);
+//sets the chosen dropdown value to the dropdown after the site reloads, so it doesnt need to be reselected
+function setSelectedDropdownOption(dropdownButtonId) {
+    var dropdownButton = document.getElementById(dropdownButtonId);
+    var parameterValue = dropdownButton.getAttribute('data-parameter-value');
+    dropdownButton.textContent = convertValueToString(dropdownButtonId, parameterValue);
+    /*if (parameterValue) {
+        dropdownButton.textContent = convertValueToString(dropdownButtonId, parameterValue);
+    } else {
+        dropdownButton.textContent = "Choose here";
+    }*/
+}
 
-        }
-    })
+//calls the setSelectedDropdownOption function for each dropdown button
+window.onload = function() {
+    //List of all dropdwon button IDs
+    var dropdownButtonIds = ['drop-down-btn-fitness', 'drop-down-btn-stamina', 'drop-down-btn-experience', 'drop-down-btn-scenery'];
 
-    const currentUrl = window.location.href;
-    window.location.href = currentUrl.split("&")[0] + `&fitness=${fitness.getAttribute('chosen-value-id')}&stamina=${stamina.value}&experience=${experience.value}&scenery=${scenery.value}&months=${monthBits}`
-    //console.log(monthBits);
+    // Call setSelectedDropdownOption for each dropdown button
+    dropdownButtonIds.forEach(function(dropdownButtonId) {
+        setSelectedDropdownOption(dropdownButtonId);
+    });
+}
+
+//converts the value of the dropdown button to a string
+function convertValueToString(type, value) {
+    switch (type) {
+        case "drop-down-btn-fitness":
+            switch (value) {
+                case "1":
+                    return "Easy";
+                case "2":
+                    return "Moderate";
+                case "3":
+                    return "Intermediate";
+                case "4":
+                    return "Expert";
+                case "5":
+                    return "Challenging";
+                default:
+                    return "Choose here";
+            }
+        case "drop-down-btn-stamina":
+            switch (value) {
+                case "1":
+                    return "Untrained";
+                case "2":
+                    return "Moderate";
+                case "3":
+                    return "Intermediate";
+                case "4":
+                    return "Athletic";
+                case "5":
+                    return "Elite";
+                default:
+                    return "Choose here";
+            }
+        case "drop-down-btn-experience":
+            switch (value) {
+                case "1":
+                    return "Novice";
+                case "2":
+                    return "Practised";
+                case "3":
+                    return "Intermediate";
+                case "4":
+                    return "Experienced";
+                case "5":
+                    return "Expert";
+                default:
+                    return "Choose here";
+            }
+        case "drop-down-btn-scenery":
+            switch (value) {
+                case "1":
+                    return "Unremarkable";
+                case "2":
+                    return "Ordinary";
+                case "3":
+                    return "Enjoyable";
+                case "4":
+                    return "Beautiful";
+                case "5":
+                    return "Stunning";
+                default:
+                    return "Choose here";
+            }
+
+    }
 }
