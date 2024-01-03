10908cb2cdb4febcfd2008566cda0210d8f3f195
Sarah Neville <sarah.neville@students.fhv.at>
2024-01-03 16:11:38 +0100

    added function to dynamically add right icon and rating to the hikes of the result list

--- src/main/webapp/JS/search.js
+++ src/main/webapp/JS/search.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // input value from search-input
         var searchString = document.querySelector(".search-input").value;
 
-       // jump to loadingSpinner
+        // jump to loadingSpinner
         window.location.href = "/Journey_war_exploded/loadingSpinner.jsp?searchString=" + encodeURIComponent(searchString);
     });
 });
@@ -16,7 +16,7 @@ function updateDropdownExIcons(dropdown, element) {
     let selectedValue = element.getAttribute("data-id");
     dropdownButton.innerHTML = element.innerHTML; // sets Button text to chosen value
     dropdownButton.setAttribute("chosen-value-id", selectedValue);
-    document.getElementById(dropdown+"-hidden").value = selectedValue;
+    document.getElementById(dropdown + "-hidden").value = selectedValue;
 
     // Highlighting the selected option so this is visible when opening dropdown again
     let dropdownItem = document.querySelector("a.dropdown-item.active");
@@ -24,4 +24,10 @@ function updateDropdownExIcons(dropdown, element) {
         dropdownItem.classList.remove("active");
     }
     element.classList.add("active");
+}
+
+//Function to dynamically add the icons and rating to the individual Hikes of the list
+function insertHikeIconAndRating(iconContainer, icon, value){
+    let container = document.getElementById(iconContainer)
+    container.innerHTML = icon+" "+value+"/5";
 }
